63246b485b9ff14c73ab0bdf4740bf163ac6395a
bradhamma50-arch <bradhamma50@gmail.com>
2026-04-08 16:42:34 +0530

    Add Tailwind CSS configuration file

---
+++ tailwind.config.js
@@ -0,0 +1,5 @@
+export default {
+    content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+    theme: { extend: { colors: { 'neon-lime': '#CCFF00', 'cyber-blue': '#00E5FF', 'obsidian': '#0A0A0A' }, fontFamily: { sans: ['Outfit', 'sans-serif'] } } },
+    plugins: [],
+}
